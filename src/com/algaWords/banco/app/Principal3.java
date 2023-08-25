9c488487fdf8e8c6264da0306e535cb1b517e723
LF-13 <l.fernandes0308@gmail.com>
2023-08-25 15:59:37 -0300

    Trabalhando com lambda e algumas formas de usar.

--- src/com/algaWords/banco/app/Principal3.java
+++ src/com/algaWords/banco/app/Principal3.java
@@ -10,10 +10,8 @@ import java.util.function.ToIntFunction;
 public class Principal3 {
     public static void main(String[] args) {
         Banco banco = new Banco();
-//        banco.getContas().sort(Comparator.comparingInt(new NumeroContaFunction()));
-        ToIntFunction<Conta> function = (Conta conta) -> {
-            return conta.getNumero();
-        };
+        banco.getContas().sort(Comparator.comparingInt(Conta::getNumero));
+
         for (Conta conta : banco.getContas()) {
             System.out.println("Agência: " + conta.getAgencia() + " / " + "Número: " + conta.getNumero());
 
