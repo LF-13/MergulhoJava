e9b38adafe7024d6251bab8c5edaa8a1e2291196
LF-13 <l.fernandes0308@gmail.com>
2023-09-19 15:58:19 -0300

    Trabalhando com Streams API:filter para filtragem de contas

--- src/com/algaWords/banco/app/Principal4.java
+++ src/com/algaWords/banco/app/Principal4.java
@@ -4,17 +4,20 @@ import com.algaWords.banco.modelo.Banco;
 import com.algaWords.banco.modelo.Conta;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 import java.util.stream.Stream;
 
 public class Principal4 {
     public static void main(String[] args) {
         Banco banco = new Banco();
 
-        Stream<Conta> stream = banco.getContas().stream();
+        Stream<Conta> stream1 = banco.getContas().stream();
 
-        stream.forEach(conta -> {
+        Stream<Conta> stream2 = stream1.filter(conta -> conta.getSaldo().compareTo(new BigDecimal("130")) > 0);
+
+        stream1.forEach(conta -> {
             conta.depositar(new BigDecimal("10"));
-            System.out.printf("Agência: %d / Número da conta: %d / Saldo: = %.2f$%n", conta.getAgencia(), conta.getNumero(),conta.getSaldo());
+            System.out.printf("Agência: %d / Número da conta: %d / Saldo: = %.2f$%n", conta.getAgencia(), conta.getNumero(), conta.getSaldo());
         });
     }
 }
