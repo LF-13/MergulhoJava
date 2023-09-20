392861adb26519a6deb8ae805a2d634fbf17d54b
LF-13 <l.fernandes0308@gmail.com>
2023-09-20 17:40:01 -0300

    Trabalhando com Streams API: Verificando o valor total do saldo em todas as contas

--- src/com/algaWords/banco/app/Principal4.java
+++ src/com/algaWords/banco/app/Principal4.java
@@ -2,16 +2,19 @@ package com.algaWords.banco.app;
 
 import com.algaWords.banco.modelo.Banco;
 import com.algaWords.banco.modelo.Conta;
-import com.algaWords.banco.modelo.Pessoa;
+
 
 import java.math.BigDecimal;
 import java.util.Comparator;
-import java.util.Objects;
-import java.util.stream.Stream;
+
 
 public class Principal4 {
     public static void main(String[] args) {
         Banco banco = new Banco();
+
+        BigDecimal saltoTotal = banco.getContas().stream().map(Conta::getSaldo)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+        System.out.printf("Salto total de todas as contas no projeto: %.2f %n%n", saltoTotal);
 
         banco.getContas().stream()
                 .filter(conta -> conta.getSaldo().compareTo(new BigDecimal("50")) > 0)
@@ -21,12 +24,12 @@ public class Principal4 {
                 .forEach(System.out::println);
 
 
-//        banco.getContas().stream()
-//                .filter(conta -> conta.getSaldo().compareTo(new BigDecimal("50")) > 0)
-//                .filter(conta -> conta.getNumero() > 200)
-//                .sorted(Comparator.comparingInt(Conta::getNumero))
-//                .forEach(conta -> {
-//                    System.out.printf("Agência: %d / Número da conta: %d / Saldo: = %.2f$%n", conta.getAgencia(), conta.getNumero(), conta.getSaldo());
-//                });
+        banco.getContas().stream()
+                .filter(conta -> conta.getSaldo().compareTo(new BigDecimal("50")) > 0)
+                .filter(conta -> conta.getNumero() > 200)
+                .sorted(Comparator.comparingInt(Conta::getNumero))
+                .forEach(conta -> {
+                    System.out.printf("Agência: %d / Número da conta: %d / Saldo: = %.2f$%n", conta.getAgencia(), conta.getNumero(), conta.getSaldo());
+                });
     }
 }
