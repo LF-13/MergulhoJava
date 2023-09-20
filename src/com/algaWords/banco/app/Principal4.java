d3c4762741e327e4c5a3d1b07b67690b3e87ee71
LF-13 <l.fernandes0308@gmail.com>
2023-09-20 17:58:07 -0300

    Trabalhando com Streams API: Usando toList.

--- src/com/algaWords/banco/app/Principal4.java
+++ src/com/algaWords/banco/app/Principal4.java
@@ -2,19 +2,24 @@ package com.algaWords.banco.app;
 
 import com.algaWords.banco.modelo.Banco;
 import com.algaWords.banco.modelo.Conta;
+import com.algaWords.banco.modelo.Pessoa;
 
 
 import java.math.BigDecimal;
 import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 public class Principal4 {
     public static void main(String[] args) {
         Banco banco = new Banco();
+        List<Pessoa> titulares = banco.getContas().stream()
+                .map(Conta::getTitular)
+                .distinct()
+                .toList();
+        System.out.printf("Titulares: %s %n%n", titulares);
 
-        BigDecimal saltoTotal = banco.getContas().stream().map(Conta::getSaldo)
-                .reduce(BigDecimal.ZERO, BigDecimal::add);
-        System.out.printf("Salto total de todas as contas no projeto: %.2f %n%n", saltoTotal);
 
         banco.getContas().stream()
                 .filter(conta -> conta.getSaldo().compareTo(new BigDecimal("50")) > 0)
@@ -23,6 +28,7 @@ public class Principal4 {
                 .distinct()
                 .forEach(System.out::println);
 
+        System.out.println("-------------------------------------------------------");
 
         banco.getContas().stream()
                 .filter(conta -> conta.getSaldo().compareTo(new BigDecimal("50")) > 0)
@@ -31,5 +37,11 @@ public class Principal4 {
                 .forEach(conta -> {
                     System.out.printf("Agência: %d / Número da conta: %d / Saldo: = %.2f$%n", conta.getAgencia(), conta.getNumero(), conta.getSaldo());
                 });
+
+
+        BigDecimal saltoTotal = banco.getContas().stream().map(Conta::getSaldo)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+        System.out.printf("%nSalto total de todas as contas no projeto: %.2f %n%n", saltoTotal);
+
     }
 }
