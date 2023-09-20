7f036093e8732fb72f48e70b3affec4038e23168
LF-13 <l.fernandes0308@gmail.com>
2023-09-20 17:09:48 -0300

    Trabalhando com Streams API: Usando map e distinct para filtragem de titulares e sem repetir titular (deixando o código mais limpo)

--- src/com/algaWords/banco/app/Principal4.java
+++ src/com/algaWords/banco/app/Principal4.java
@@ -2,8 +2,10 @@ package com.algaWords.banco.app;
 
 import com.algaWords.banco.modelo.Banco;
 import com.algaWords.banco.modelo.Conta;
+import com.algaWords.banco.modelo.Pessoa;
 
 import java.math.BigDecimal;
+import java.util.Comparator;
 import java.util.Objects;
 import java.util.stream.Stream;
 
@@ -11,8 +13,20 @@ public class Principal4 {
     public static void main(String[] args) {
         Banco banco = new Banco();
 
-        banco.getContas().stream().filter(conta -> conta.getSaldo().compareTo(new BigDecimal("130")) > 0).filter(conta -> conta.getNumero() > 300).forEach(conta -> {
-            System.out.printf("Agência: %d / Número da conta: %d / Saldo: = %.2f$%n", conta.getAgencia(), conta.getNumero(), conta.getSaldo());
-        });
+        banco.getContas().stream()
+                .filter(conta -> conta.getSaldo().compareTo(new BigDecimal("50")) > 0)
+                .filter(conta -> conta.getNumero() > 200)
+                .map(Conta::getTitular)
+                .distinct()
+                .forEach(System.out::println);
+
+
+//        banco.getContas().stream()
+//                .filter(conta -> conta.getSaldo().compareTo(new BigDecimal("50")) > 0)
+//                .filter(conta -> conta.getNumero() > 200)
+//                .sorted(Comparator.comparingInt(Conta::getNumero))
+//                .forEach(conta -> {
+//                    System.out.printf("Agência: %d / Número da conta: %d / Saldo: = %.2f$%n", conta.getAgencia(), conta.getNumero(), conta.getSaldo());
+//                });
     }
 }
