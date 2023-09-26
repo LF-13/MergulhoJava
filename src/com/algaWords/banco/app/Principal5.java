e23d3b6eca03429addc50376adf6853cc2fc44db
LF-13 <l.fernandes0308@gmail.com>
2023-09-26 16:34:05 -0300

    Trabalhando com API Optional: orElseThrow

--- src/com/algaWords/banco/app/Principal5.java
+++ src/com/algaWords/banco/app/Principal5.java
@@ -9,8 +9,11 @@ public class Principal5 {
     public static void main(String[] args) {
         Banco banco = new Banco();
 
-        Optional<Conta> contaOptional = banco.buscar(123, 1234);
-        contaOptional.ifPresent(conta -> System.out.println(conta.getSaldo()));
+//        Optional<Conta> contaOptional = banco.buscar(123, 1234);
+//        contaOptional.ifPresent(conta -> System.out.println(conta.getSaldo()));
 
+        Conta contaEncontrada = banco.buscar(123, 123)
+                .orElseThrow(() -> new RuntimeException("CONTA NÃO ENCONTRADA."));
+        System.out.println(contaEncontrada.getSaldo());
     }
 }
